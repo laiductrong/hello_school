a1682d572d2b55e01f9e32165cdbb2c1eb034664
Lại Trọng <trong39a2pct2018@gmail.com>
2024-07-17 03:13:03 +0700

    protect router link

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private auth: AuthService) {
+    
+  }
+  public role: any;
+  ngOnInit(): void {
+    this.role = this.auth.getUserRole();
+  }
   toggleMenu(): void {
     const element = document.querySelector('.topnav');
     if (element?.classList.contains('responsive')) {
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,24 +4,33 @@ import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-    const router = inject(Router);
+  const router = inject(Router);
 
-    if (authService.isAuthenticated() == false) {
-        router.navigate(['/account']);
-        return false;
+  if (authService.isAuthenticated() == false) {
+    router.navigate(['/account']);
+    return false;
+  }
+  if (state.url.includes('/academic')) {
+    if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
+      return true;
+
     }
-    if (state.url.includes('/academic')) {
-      if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
-        return true;
-      }}
-      if(state.url.includes('/student')) {
-        if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
-          return true;
-      }}
-      if(state.url.includes('/class')) {
-        if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
-          return true;
-      }}
+    return false;
+  }
+  if (state.url.includes('/student')) {
+    if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
+      return true;
 
-    return true;
+    }
+    return false;
+  }
+  if (state.url.includes('/class')) {
+    if (authService.getUserRole() === 'Teacher' || authService.getUserRole() === 'Manager') {
+      return true;
+
+    }
+    return false;
+  }
+
+  return true;
 };
